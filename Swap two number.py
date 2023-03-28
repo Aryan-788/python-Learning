740f250d3f6c416fc8e0c517f268b0cb97a2f3bf
Aryan Narayan <122514284+Aryan-788@users.noreply.github.com>
2023-03-28 23:13:31 +0530

    Add files via upload

---
+++ Swap two number.py
@@ -0,0 +1,10 @@
+P = int(input("Please enter value for P:  "))
+Q = int(input("Please enter value for Q:  "))
+print("****Before Swapping****")
+print("The value of P is ",P)
+print("The value of Q is ",Q)
+
+P, Q = Q, P
+print("****After Swapping****")
+print("The value of P is ",P)
+print("The value of Q is ",Q)
