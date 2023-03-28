042d7638db56d1f40386c2c6ac75441b238a66b9
Aryan Narayan <122514284+Aryan-788@users.noreply.github.com>
2023-03-28 23:22:42 +0530

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,8 @@
+print("Enter your age:- ")
+age = int(input())
+if age < 18:
+    print("you cannot drive")
+elif age == 18:
+    print("firstly you take permission to me than you are eligible for driving")
+else:
+    print("you can drive a car")
