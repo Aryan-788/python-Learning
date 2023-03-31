c06b6fab50a663ce920cddc467c2ea783484e886
Aryan Narayan <122514284+Aryan-788@users.noreply.github.com>
2023-03-31 23:32:23 +0530

    Create Sum_of_two_num.py

---
+++ Sum_of_two_num.py
@@ -0,0 +1,14 @@
+print("Enter your first numbers :- ")
+inpnum1 = input()
+
+print("Enter your second numbers :- ")
+inpnum2 = input()
+
+print("The sum of two number is ", int(inpnum1) + int(inpnum2))
+
+print("____Another Method____")
+
+num1 = int(input("Enter the 1st Number :- "))
+num2 = int(input("Enter the 2nd Number :- "))
+num3 = num1 + num2
+print("The Sum of Two Number is :-", num3)
