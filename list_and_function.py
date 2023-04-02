da03b75200e0f8f19d30e1575a9a614aa17cf619
Aryan Narayan <122514284+Aryan-788@users.noreply.github.com>
2023-04-02 23:56:48 +0530

    Create list_and_function.py

---
+++ list_and_function.py
@@ -0,0 +1,34 @@
+# grocery = ["pen", "harpic", "bhindi", "lollypop", "copy"]
+# print(grocery)
+# numbers = [2,34,4,65,76,98]      # ye list h....... iski value change ho jati h
+# numbers.sort()
+# numbers.reverse()
+# print(numbers[4])
+# print(numbers[3:5])
+# print(max(numbers))
+# print(min(numbers))
+# numbers.append(8)      #ye koi v array ke last index me number add kr deta h
+# numbers.append(4)
+# numbers.append(24)
+# numbers.insert(1,57)       # ye koi v array ke andar kahi pr kuch v data store kr sakta h aapko jaisa  need ho
+# numbers.remove(4)     # ye index ke data ko remove krta h
+# numbers.pop()       # ye last index ko remove krta h
+# numbers[2] = 23     # ye koi v index ke value ko change krta h
+# print(numbers)
+
+# mutable  - can change
+# immutable  - cannot change
+# tp = (1,2,3)     # ye tuple h.........
+# tp[1] = 8          #  iski value change ni ho sakti
+# print(tp)
+a = 1
+b = 3
+a , b = b , a       # easy method for # interchanging the two number in python
+# temp = a          # interchanging number a = b and b = a
+# a = b
+# b = temp
+print(a, b)
+
+
+
+
