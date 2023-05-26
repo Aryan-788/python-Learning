585412e6b909645b9b4a9eb06a3b55536375aa57
Aryan Narayan <122514284+Aryan-788@users.noreply.github.com>
2023-05-26 16:49:29 +0530

    Create Table-using-for-loop.py

---
+++ Table-using-for-loop.py
@@ -0,0 +1,9 @@
+'''
+Your little brother has just learned multiplication tables in school. Today he has learned tables of 6 and 7. Help him memorise both the tables by printing them using for loop.
+'''
+print("Multiplication table of 6:")
+for i in range (1,11):
+    print("6 *",i,"=",6*i)
+print("Multiplication table of 7:")
+for i in range (1,11):
+    print("7 *",i,"=",7*i)
